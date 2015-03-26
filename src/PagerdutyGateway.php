b164271a22d1f3062280298e6eff491350e58919
cameronevans <cameron@toplog.io>
2015-03-26 13:18:44 -0300

    fixes a couple errors

--- src/PagerdutyGateway.php
+++ src/PagerdutyGateway.php
@@ -76,7 +76,7 @@ class PagerdutyGateway implements GatewayInterface
     public function notify($to, $message, array $options = [])
     {
         $options['to'] = $to;
-
+        $params = [];
         $params = $this->addMessage($message, $params, $options);
 
         return $this->commit('post', $this->buildUrlFromString("create_event.json"), $params);
@@ -154,7 +154,7 @@ class PagerdutyGateway implements GatewayInterface
     {
         return (new Response())->setRaw($response)->map([
             'success' => $success,
-            'message' => $success ? 'Message sent' : $response['error']['message'],
+            'message' => $success ? 'Message sent' : $response['error'],
         ]);
     }
 
